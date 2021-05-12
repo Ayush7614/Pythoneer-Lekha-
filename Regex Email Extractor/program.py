712780f9c7ce072cc72dbca90f97c3131cf9bc32
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-05-12 21:46:26 +0530

    Update program.py

--- Regex Email Extractor/program.py
+++ Regex Email Extractor/program.py
@@ -1,1 +1,18 @@
+# Python program to extract emails From
+# the String By Regular Expression.
 
+# Importing module required for regular
+# expressions
+import re
+
+# Example string
+s = """Hello from xxxxxxxxxx@gmail.com
+		to xxxxxxx@.com about the meeting @2PM"""
+
+# \S matches any non-whitespace character
+# @ for as in the Email
+# + for Repeats a character one or more times
+lst = re.findall('\S+@\S+', s)	
+
+# Printing of List
+print(lst)
